c97b0fc3f31946cf20c4cc179a32bd06dc211c22
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-20 23:20:43 +0900

    Chore: remove useless files

--- hooks/useCustomRouter.ts
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/router';
-
-export default function useCustomRouter(path: string) {
-  const router = useRouter();
-  const onRouteToPath = () => router.push(path);
-  return { onRouteToPath };
-}
