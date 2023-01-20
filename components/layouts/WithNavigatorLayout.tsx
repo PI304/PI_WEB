1470a51f6b655de1a144c58a7f9542d5f7f29b26
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-21 00:16:02 +0900

    Feat: with navigator layout

---
+++ components/layouts/WithNavigatorLayout.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { DefaultLayout } from '.';
+import { Navigator } from '../shared';
+
+export default function WithNavigatorLayout({ children }: ChildrenType) {
+  return (
+    <DefaultLayout>
+      <S.Layout>
+        <Navigator />
+        <section>{children}</section>
+      </S.Layout>
+    </DefaultLayout>
+  );
+}
+
+namespace S {
+  export const Layout = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+
+    > section {
+      flex-grow: 1;
+    }
+  `;
+}
