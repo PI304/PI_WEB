7a4aae77024fcaf7cbf7d5e26a670ecfb950c452
sookyeong <sookyeong.dev@gmail.com>
2023-01-23 01:15:58 +0900

    Feature/atomic typography (#4)
    * Feat: consolas as default font family
    
    * Feat: default atomic typography

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,1 +1,11 @@
-export const Fonts = {} as const;
+export const Fonts = {
+  /* 400 */
+  regular32: 'font-size:3.2rem; font-weight:400;',
+
+  /* 700 */
+  bold30: 'font-size:3rem; font-weight:700;',
+  bold40: 'font-size:4rem; font-weight:700;',
+  bold70: 'font-size:7rem; font-weight:700;',
+  bold100: 'font-size:10rem; font-weight:700;',
+  bold150: 'font-size:15rem; font-weight:700;',
+} as const;
