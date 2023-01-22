03f17fa00c092ab9196d60ccc3985b4d83166058
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-23 01:14:26 +0900

    Feat: consolas as default font family

--- styles/global.ts
+++ styles/global.ts
@@ -15,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
   * {
     font-size: 1.6rem;
     font-weight: 400;
+    font-family: Consolas, monaco, monospace;
     box-sizing: border-box;
   }
 `;
