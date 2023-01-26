4b703480ad145f0061d7b1d62273ffb6c4059f2d
dmsthf2000 <dsmthf200099@gmail.com>
2023-01-26 15:32:50 +0900

    Feat: 404페이지 UI 개발, errorUFO svg 추가

---
+++ pages/404.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { SpaceBackground } from '../components/shared';
+import { Colors, Fonts, svg404Ufo } from '../styles';
+
+export default function NotFoundPage() {
+  return (
+    <S.Container>
+      <S.ErrorImg>{svg404Ufo}</S.ErrorImg>
+      <S.ErrorText>404</S.ErrorText>
+      <S.ErrorText>ERROR</S.ErrorText>
+      <SpaceBackground />
+    </S.Container>
+  );
+}
+
+namespace S {
+  export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vm;
+    height: 100vh;
+  `;
+
+  export const ErrorImg = styled.div`
+    margin: 0 auto;
+    animation: bounce 0.5s linear 0s infinite alternate;
+    backdrop-filter: blur(2px);
+
+    @keyframes bounce {
+      0% {
+        transform: translateY(0);
+      }
+      100% {
+        transform: translateY(-1rem);
+      }
+    }
+  `;
+
+  export const ErrorText = styled.div`
+    color: ${Colors.white};
+    margin: 0.5rem auto;
+    text-align: center;
+    ${Fonts.bold70}
+  `;
+}
