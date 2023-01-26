171c0ec111dcdd0e3faddeb63601db683a2eea13
sookyeong <sookyeong.dev@gmail.com>
2023-01-26 19:37:18 +0900

    Feature/404 refactor (#8)
    * Refactor: re-export without widget layout
    
    * Feat: seo title for not found
    
    * Refactor: apply re-export & custom head to not found page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,13 @@
 import { ReactElement } from 'react';
-import { WithoutWidgetLayout } from '../components/layouts/WithoutWidgetLayout';
+import { WithoutWidgetLayout } from '../components/layouts';
 import { NotFoundPage } from '../components/pages/notFound';
+import { CustomHead } from '../components/seo';
+import { Seo } from '../constants/seo';
 
 export default function _404Page() {
   return (
     <>
+      <CustomHead title={Seo.Title.notFound} />
       <NotFoundPage />
     </>
   );
