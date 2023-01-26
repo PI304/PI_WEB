b293bad96c6bb1e2f23e514e1c96b57725c118ba
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-27 05:18:18 +0900

    Feat: navigator phases as constants

---
+++ constants/phases.ts
@@ -0,0 +1,22 @@
+export const Phases = {
+  1: {
+    startPoint: 0,
+    willMove: 600,
+    scale: 1.4,
+  },
+  2: {
+    startPoint: 600,
+    willMove: 500,
+    scale: 0.7,
+  },
+  3: {
+    startPoint: 1100,
+    willMove: 410,
+    scale: 0.3,
+  },
+  4: {
+    startPoint: 1510,
+    willMove: 571.4,
+    scale: 0.35,
+  },
+} as const;
