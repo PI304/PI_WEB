59136d2c9c1dbc7d057c56c9f1fa858e8029ac09
dmsthf2000 <dsmthf200099@gmail.com>
2023-02-05 04:50:33 +0900

    Feat: main text animation

--- components/pages/main/TextAnimation.tsx
+++ components/pages/main/TextAnimation.tsx
@@ -23,31 +23,31 @@ export const TextAnimation = () => {
 
   return (
     <S.Container>
-      {/*<S.AndText></S.AndText>*/}
-      <S.WeText></S.WeText>
       {timer <= 5 ? (
-        <S.TextDiv>
-          <S.RotateBox action={'frontToTop'} length={wordArr[count].length}>
-            {wordArr[count]}
-          </S.RotateBox>
-          <S.RotateBox action={'bottomToFront'} length={wordArr[count + 1].length}>
-            {wordArr[count + 1]}
-          </S.RotateBox>
-          <S.RotateBox action={'backToBottom'} length={wordArr[count + 2].length}>
-            {wordArr[count + 2]}
-          </S.RotateBox>
-          <S.RotateBox action={'topToBack'} length={wordArr[3].length}>
-            {wordArr[3]}
-          </S.RotateBox>
-        </S.TextDiv>
+        <>
+          <S.WeText></S.WeText>
+          <S.TextDiv>
+            <S.RotateBox action={'frontToTop'} length={wordArr[count].length}>
+              {wordArr[count]}
+            </S.RotateBox>
+            <S.RotateBox action={'bottomToFront'} length={wordArr[count + 1].length}>
+              {wordArr[count + 1]}
+            </S.RotateBox>
+            <S.RotateBox action={'backToBottom'} length={wordArr[count + 2].length}>
+              {wordArr[count + 2]}
+            </S.RotateBox>
+            <S.RotateBox action={'topToBack'} length={wordArr[3].length}>
+              {wordArr[3]}
+            </S.RotateBox>
+          </S.TextDiv>
+        </>
       ) : (
         timer >= 5 && (
-          <S.TextContainer>
-            <S.ThisText>This is How We PLAY..</S.ThisText>
+          <S.ThisText>
+            <S.RemoveText>This Is How We </S.RemoveText>
+            <S.PlayText>PLAY</S.PlayText>
             <S.TypingText>IDEA</S.TypingText>
-
-            {/*<S.PlayText>PLAY</S.PlayText>*/}
-          </S.TextContainer>
+          </S.ThisText>
         )
       )}
     </S.Container>
@@ -56,22 +56,12 @@ export const TextAnimation = () => {
 
 namespace S {
   export const Container = styled.div`
-    width: 100%;
-    height: 100%;
     position: absolute;
     display: flex;
-    //flex-direction: column;
     align-items: center;
     justify-content: center;
   `;
 
-  export const AndText = styled.div`
-    ${Fonts.bold70};
-    background: ${Colors.white};
-    color: ${Colors.black900};
-    //opacity: 0;
-    display: flex;
-  `;
   export const WeText = styled.div`
     height: 7rem;
     margin: 0 1rem;
@@ -82,11 +72,9 @@ namespace S {
   `;
 
   export const TextDiv = styled.div`
-    //float: left;
     height: 7rem;
     color: ${Colors.black900};
     width: 33rem;
-    //position: absolute;
     transform-style: preserve-3d;
     animation: rotate 4s ease 1s forwards 1;
 
@@ -128,107 +116,115 @@ namespace S {
     transform: ${(props) => props.action === 'topToBack' && 'rotateX(90deg) translateZ(3.5rem)'};
   `;
 
-  export const TextContainer = styled.div`
-    //width: 40%;
-    overflow: hidden;
-    white-space: nowrap;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-  `;
-
   export const ThisText = styled.div`
     ${Fonts.bold70};
-    color: ${Colors.black900};
-    background: ${Colors.white};
-    //display: inline-block;
-    position: absolute;
-    //text-align: center;
-    animation: leftRight 2s forwards 1 ease-in-out;
+    color: ${Colors.white};
+    display: inline-flex;
+    white-space: nowrap;
+    animation: 1.5s anim-lineUp ease-out forwards;
 
-    @keyframes leftRight {
-      0%,
-      20% {
-        transform: translateX(0%);
-        right: 0;
+    @keyframes anim-lineUp {
+      0% {
+        opacity: 0;
+        transform: translateY(80%);
       }
-      80%,
+      20% {
+        opacity: 0;
+      }
+      50% {
+        opacity: 1;
+        transform: translateY(0%);
+      }
       100% {
-        //width: 40%;
-        transform: translateX(25%);
-        right: 100%;
-        //width: 20%;
+        opacity: 1;
+        transform: translateY(0%);
+      }
     }
   `;
 
+  export const PlayText = styled.span`
+    :first-letter {
+      color: #956eb5;
+    }
+  `;
   export const TypingText = styled.span`
-    ${Fonts.bold70};
-    color: ${Colors.black900};
-    background: ${Colors.white};
-    animation: type 1620ms;
-    animation-fill-mode: forwards;
-    animation-delay: 1s;
-
-    :after {
-      animation: cursor 320ms 8.625 ease-in-out;
+    width: 0;
+    animation: type 1000ms ease-in-out 4500ms forwards;
+    overflow: hidden;
+    :first-letter {
+      color: #956eb5;
     }
 
-    @keyframes cursor {
-      0%,
-      100% {
-        border-color: #f7f3ff;
+    @keyframes type {
+      25% {
+        width: 4.2rem;
       }
       50% {
-        border-color: #241b5b;
+        width: 8.4rem;
+      }
+      75% {
+        width: 12.6rem;
+      }
+      100% {
+        width: 16.8rem;
+      }
+    }
+  `;
+
+  export const RemoveText = styled.span`
+    width: 63rem;
+    animation: remove 1500ms ease-in-out 2500ms forwards;
+    overflow: hidden;
+
+    @keyframes remove {
+      6.6% {
+        width: 63rem;
+      }
+      13.2% {
+        width: 58.8rem;
+      }
+      19.8% {
+        width: 54.6rem;
+      }
+      26.4% {
+        width: 50.4rem;
+      }
+      33% {
+        width: 46.2rem;
+      }
+      39.6% {
+        width: 42rem;
+      }
+      46.2% {
+        width: 37.8rem;
+      }
+      52.8% {
+        width: 33.6rem;
+      }
+      59.4% {
+        width: 29.4rem;
+      }
+      66% {
+        width: 25.2rem;
+      }
+      72.6% {
+        width: 21rem;
+      }
+      79.2% {
+        width: 16.8rem;
+      }
+      85.8% {
+        width: 12.6rem;
+      }
+      92.4% {
+        width: 8.4rem;
+      }
+      99% {
+        width: 4.2rem;
       }
       100% {
         width: 0;
       }
     }
-
-    @keyframes type {
-      11.1111111111% {
-        width: 0.49em;
-      }
-      22.2222222222% {
-        width: 0.98em;
-      }
-      33.3333333333% {
-        width: 1.47em;
-      }
-      44.4444444444% {
-        width: 1.96em;
-      }
-      55.5555555556% {
-        width: 2.45em;
-      }
-      66.6666666667% {
-        width: 2.94em;
-      }
-      77.7777777778% {
-        width: 3.43em;
-      }
-      88.8888888889% {
-        width: 3.92em;
-      }
-      100% {
-        width: 3.92em;
-        padding-right: 0;
-      }
-    }
   `;
-
-  // export const IdeaText = styled.div`
-  //   ${Fonts.bold70};
-  //   color: darkred;
-  //   animation: moveLeft 1s linear;
-  //   @keyframes moveLeft {
-  //     0% {
-  //       transform: translateX(500px);
-  //     }
-  //     100% {
-  //       transform: translateX(-200px);
-  //     }
-  //   }
-  // `;
 }
